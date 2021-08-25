1afe2e63a5a4fdac90d2bb281717b94e494a2add
kignatov <kignatov@rambler-co.ru>
2021-08-25 21:49:02 +0300

    add silence detect

--- transcoding.sh
+++ transcoding.sh
@@ -41,29 +41,41 @@ ABRATE="128k"
 ASAMPLING="48k"
 LOGFILE=$(mktemp /tmp/ffmpeg.XXXXXXXXX)
 TMPFILE=$(mktemp /tmp/ffmpeg-pass-1.XXXXXXXXX)
+TMP_SILENCE_FILE=$(mktemp /tmp/ffmpeg-silence.XXXXXXXXX)
 LOUDNORM="-af loudnorm=I=-16:TP=-1:LRA=13"
 LOUDNORM_PARAMS=":print_format=json"
+LOUDNORM_OFF=0
 
-if [[ ! ${VBRATE} ]]; then echo 'ERROR. undefined --bitrate opt';exit; fi
-if [[ ! ${VSCALE} ]]; then echo 'ERROR. undefined --scale opt';exit; fi
-if [[ ! ${RESULT_FILENAME_PATH} ]]; then echo 'ERROR. undefined --output opt';exit; fi
+    if [[ ! ${VBRATE} ]]; then echo 'ERROR. undefined --bitrate opt';exit; fi
+    if [[ ! ${VSCALE} ]]; then echo 'ERROR. undefined --scale opt';exit; fi
+    if [[ ! ${RESULT_FILENAME_PATH} ]]; then echo 'ERROR. undefined --output opt';exit; fi
 
-if [[ ${DEBUG} -eq 1 ]]; then
-  # shellcheck disable=SC2006
-  finfo=`$ffprobe -v error -select_streams v:0 -show_entries format=bit_rate,duration -show_entries stream=width,height -of default=noprint_wrappers=1 ${input_file} |tr -s '\n' ','`
-  width=$(echo $finfo | cut -f1 -d ',' | cut -f2 -d'=')
-  height=$(echo $finfo | cut -f2 -d ',' | cut -f2 -d'=')
-  bitrate=$(echo $finfo | cut -f4 -d ',' | cut -f2 -d'=')
-  duration=$(echo $finfo | cut -f3 -d ',' | cut -f2 -d'=')
-  size=$(du -h ${input_file} |cut -f1)
-  yes "*"  |head -n 80|tr -d '\n'
-  echo
-  echo "INPUT fileinfo: scale=${width}x${height}, bitrate=$((bitrate/1000))kb/s, duration: ${duration%.*}sec, size: ${size}"
-  yes "*"  |head -n 80|tr -d '\n'
-  echo
-fi
+    # shellcheck disable=SC2006
+    finfo=`$ffprobe -v error -select_streams v:0 -show_entries format=bit_rate,duration -show_entries stream=width,height -of default=noprint_wrappers=1 ${input_file} |tr -s '\n' ','`
+    width=$(echo $finfo | cut -f1 -d ',' | cut -f2 -d'=')
+    height=$(echo $finfo | cut -f2 -d ',' | cut -f2 -d'=')
+    bitrate=$(echo $finfo | cut -f4 -d ',' | cut -f2 -d'=')
+    duration=$(echo $finfo | cut -f3 -d ',' | cut -f2 -d'=')
+    size=$(du -h ${input_file} |cut -f1)
 
-if [[ ${DEBUG} -eq 1 ]]; then echo 'DEBUG. TWO PASS CODING START'; fi
+    if [[ ${DEBUG} -eq 1 ]]; then
+      yes "*"  |head -n 80|tr -d '\n'
+      echo
+      echo "INPUT fileinfo: scale=${width}x${height}, bitrate=$((bitrate/1000))kb/s, duration: ${duration%.*}sec, size: ${size}"
+      yes "*"  |head -n 80|tr -d '\n'
+      echo
+    fi
+
+    # detect silence
+    $ffmpeg -i ${input_file} -af silencedetect=noise=0.0001  -f null /dev/null 2> "${TMP_SILENCE_FILE}"
+    silence_duration=$(cat ${TMP_SILENCE_FILE} | grep silence_duration | cut -f2 -d'|' |  cut -f2 -d':' |tr -d ' ')
+    int_duration=$(echo "$duration" | awk '{printf "%.0f", $1}')
+    if [[ ${DEBUG} -eq 1 ]]; then echo "DEBUG. duration:$int_duration <> silence: ${silence_duration:-0}"; fi
+    if ((int_duration - silence_duration < 2));then
+      LOUDNORM_OFF=1
+      if [[ ${DEBUG} -eq 1 ]]; then echo "DEBUG. LOUDNORM OFF"; fi
+    fi
+
 		SCALE="scale=${VSCALE}"
 		BITRATE="${VBRATE}k"
 		start=$(date +%s)
@@ -74,36 +86,37 @@ if [[ ${DEBUG} -eq 1 ]]; then echo 'DEBUG. TWO PASS CODING START'; fi
       echo "DEBUG. output filename: ${RESULT_FILENAME_PATH}"
     fi
 
+    if [[ ${DEBUG} -eq 1 ]]; then echo 'DEBUG. TWO PASS CODING START'; fi
     # pass 1
 		$ffmpeg -y -i ${input_file} ${COMMAND} ${LOUDNORM}${LOUDNORM_PARAMS} -vsync cfr -pass 1 -f null /dev/null 2> ${TMPFILE}
 
 		pass1=$(date +%s)
 		runtime=$((pass1 -start))
     if [[ ${DEBUG} -eq 1 ]]; then echo "DEBUG. PASS-1 done - ${runtime}sec"; fi
-
-		# shellcheck disable=SC2006
-		LOUDNORM_PARAMS=`cat ${TMPFILE} |sed -n  '/Parsed_loudnorm/,/}/p' | tail -n+3 |head -n+10| tr -d '",' | while IFS='' read -r line || [[ -n "$line" ]]; do
-			val=$(echo "${line}" | tr -d '[:space:]' | cut -f2 -d':')
-			case $(echo "${line}" | tr -d '[:space:]'  | cut -f1 -d':') in
-			     "input_i")
-			          l_param="${l_param}:measured_I=${val}"
-			          ;;
-			     "input_tp")
-			          l_param="${l_param}:measured_TP=${val}"
-			          ;;
-			     "input_lra")
-			          l_param="${l_param}:measured_LRA=${val}"
-			          ;;
-			     "input_thresh")
-			          l_param="${l_param}:measured_thresh=${val}"
-			          ;;
-			     "target_offset")
-			          l_param="${l_param}:offset=${val}:linear=true"
-			          ;;
-			esac
-			echo "${l_param}"
-		done | tail -1`
-
+    if [[ ${LOUDNORM_OFF} -eq 0 ]]; then
+      # shellcheck disable=SC2006
+      LOUDNORM_PARAMS=`cat ${TMPFILE} |sed -n  '/Parsed_loudnorm/,/}/p' | tail -n+3 |head -n+10| tr -d '",' | while IFS='' read -r line || [[ -n "$line" ]]; do
+        val=$(echo "${line}" | tr -d '[:space:]' | cut -f2 -d':')
+        case $(echo "${line}" | tr -d '[:space:]'  | cut -f1 -d':') in
+             "input_i")
+                  l_param="${l_param}:measured_I=${val}"
+                  ;;
+             "input_tp")
+                  l_param="${l_param}:measured_TP=${val}"
+                  ;;
+             "input_lra")
+                  l_param="${l_param}:measured_LRA=${val}"
+                  ;;
+             "input_thresh")
+                  l_param="${l_param}:measured_thresh=${val}"
+                  ;;
+             "target_offset")
+                  l_param="${l_param}:offset=${val}:linear=true"
+                  ;;
+        esac
+        echo "${l_param}"
+      done | tail -1`
+    fi
     # debug message
     if [[ ${DEBUG} -eq 1 ]]; then echo "DEBUG. LOUDNORM_PARAMS = ${LOUDNORM_PARAMS}"; fi
 
@@ -118,6 +131,6 @@ if [[ ${DEBUG} -eq 1 ]]; then echo 'DEBUG. TWO PASS CODING START'; fi
       echo "DEBUG. PASS-2 done - ${runtime}sec (${p2time}sec)";
      fi
 
-    rm ${LOGFILE} ${TMPFILE}
+    rm ${LOGFILE} ${TMPFILE} ${TMP_SILENCE_FILE}
 
     echo 'Done'
